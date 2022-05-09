b9d5d61f03bee3c15e7ed31ad177d7c742479ab7
Sule Aishat Aladi <suleaishat990@gmail.com>
2022-05-09 07:56:00 +0100

    first_update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Salmart</a>
-      </header>
-      <main>List products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Salmart</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+//an object to return sample products
+
+const data = {
+  products: [
+    {
+      name: 'Igala Wrapper',
+      slug: 'igala-wrapper',
+      category: 'Traditional Wears',
+      image: '/images/igala.jpg',
+      price: 120,
+      countInStock: 40,
+      brand: 'Igala Wears',
+      rating: 4.9,
+      numReviews: 10,
+      description: 'High quality wears worn by the Igala nation',
+    },
+    {
+      name: 'Tiv Wrapper',
+      slug: 'Tiv-wrapper',
+      category: 'Traditional Wears',
+      image: '/images/tiv.jpg',
+      price: 120,
+      countInStock: 40,
+      brand: 'Tiv Wears',
+      rating: 4.9,
+      numReviews: 10,
+      description: 'High quality wears worn by the Tiv nation',
+    },
+    {
+      name: 'Idoma Wrapper',
+      slug: 'idoma-wrapper',
+      category: 'Traditional Wears',
+      image: '/images/idoma.jpg',
+      price: 120,
+      countInStock: 40,
+      brand: 'Idoma Wears',
+      rating: 4.9,
+      numReviews: 10,
+      description: 'High quality wears worn by the Idoma nation',
+    },
+    {
+      name: 'Igede Wrapper',
+      slug: 'igede-wrapper',
+      category: 'Traditional Wears',
+      image: '/images/igede.jpg', //679px x 829px
+      price: 120,
+      countInStock: 40,
+      brand: 'Igede Wears',
+      rating: 4.9,
+      numReviews: 10,
+      description: 'High quality wears worn by the Igede nation',
+    },
+  ],
+};
+
+export default data;
