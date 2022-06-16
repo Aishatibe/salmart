64e35f16fe2fef78b3b59e3d6bcfc718a0c0d663
Sule Aishat Aladi <suleaishat990@gmail.com>
2022-06-16 15:16:55 +0100

    second commit

--- backend/data.js
+++ backend/data.js
@@ -1,8 +1,26 @@
 //an object to return sample products
+import bcrpt from 'bcryptjs';
 
 const data = {
+  users: [
+    {
+      name: 'Aishat',
+      email: 'ibe990@gmail.com',
+      password: bcrpt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Kunle',
+      email: 'kunledada@gmail.com',
+      password: bcrpt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
+      //set ID as _id to make it compatible with mongoDB
+      //_id: '1',
       name: 'Igala Wrapper',
       slug: 'igala-wrapper',
       category: 'Traditional Wears',
@@ -15,6 +33,7 @@ const data = {
       description: 'High quality wears worn by the Igala nation',
     },
     {
+      // _id: '2',
       name: 'Tiv Wrapper',
       slug: 'Tiv-wrapper',
       category: 'Traditional Wears',
@@ -27,6 +46,7 @@ const data = {
       description: 'High quality wears worn by the Tiv nation',
     },
     {
+      // _id: '3',
       name: 'Idoma Wrapper',
       slug: 'idoma-wrapper',
       category: 'Traditional Wears',
@@ -39,12 +59,13 @@ const data = {
       description: 'High quality wears worn by the Idoma nation',
     },
     {
+      // _id: '4',
       name: 'Igede Wrapper',
       slug: 'igede-wrapper',
       category: 'Traditional Wears',
       image: '/images/igede.jpg', //679px x 829px
       price: 150,
-      countInStock: 40,
+      countInStock: 0,
       brand: 'Igede Wears',
       rating: 3.9,
       numReviews: 12,
